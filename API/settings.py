add2f4e2d6c088e36ee1844aedfa3496e149962e
Ryan Dailey <ryan@localhost.localdomain>
2017-03-02 15:01:13 -0800

    fixing

--- API/settings.py
+++ API/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,16 +76,7 @@ WSGI_APPLICATION = 'API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cam2api',
-        'USER': 'cam2api',
-        'PASSWORD': 'no1234',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
