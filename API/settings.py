56ca203f1f771fc2d226fd0a407e14855d44fe53
Ryan Dailey <ryan@localhost.localdomain>
2017-03-01 16:36:28 -0800

    Fixed Static Files

--- API/settings.py
+++ API/settings.py
@@ -123,4 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = './API/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = './API/static'
