f1ea7b58de2ecb9de70edd541260e1393cb9a73a
Ryan Dailey <ryan@localhost.localdomain>
2017-03-01 17:29:09 -0800

    fixing

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ryan/Django/API/env"
+set -gx VIRTUAL_ENV "/home/ryan/CAM2API/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
