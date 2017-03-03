2c9dfd7039c8c63734fbaab1547cff8c56dea7f3
Ryan Dailey <ryan@localhost.localdomain>
2017-03-02 16:34:26 -0800

    Seperated Settings

--- API/settings.py
+++ API/settings.py
@@ -55,5 +55,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = './API/static'
 
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
+
 # Default Set of DEBUG is False
 DEBUG = True
