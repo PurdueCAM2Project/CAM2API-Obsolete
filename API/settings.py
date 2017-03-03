ae8e8278291accd076b7ef94c5e2a192b7292621
Ryan Dailey <ryan@localhost.localdomain>
2017-03-02 17:25:31 -0800

    Fixed Static File Collection

--- API/settings.py
+++ API/settings.py
@@ -53,9 +53,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    )
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+#     )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
