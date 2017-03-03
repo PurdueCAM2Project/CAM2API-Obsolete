2bc91c3a67cd30bdb585828d5a09d1f0ab1bd253
Ryan Dailey <ryan@localhost.localdomain>
2017-03-02 16:45:02 -0800

    Collected Static Files

--- API/settings.py
+++ API/settings.py
@@ -52,7 +52,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Static asset configuration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
+STATIC_URL = './API/static/'
 STATIC_ROOT = './API/static'
 
 STATIC_DIRS = (
