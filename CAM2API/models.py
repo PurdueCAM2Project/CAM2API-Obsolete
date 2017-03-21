5f5b8ed14b60ca8e08b01650cd0c30ede108d522
Ryan Dailey <ryan@localhost.localdomain>
2017-03-21 15:34:48 -0700

    Major Models update adding retrevial Models

--- CAM2API/models.py
+++ CAM2API/models.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
 
 # from django.db import models
-from django.contrib.gis.db import models #as gisModels
+from django.contrib.gis.db import models # contrib.gis.db ensures the PostGis models are included
 
-# Create your models here.
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
+# Camera Model
 class Camera(models.Model):
-	camera_id = models.PositiveIntegerField() # id from old database 
+	camera_id = models.PositiveIntegerField(unique=True) # id from old database 
 	# Geography: 
 	city = models.CharField(max_length=30, null=False)
 	state = models.CharField(max_length=12, null=True)
@@ -13,10 +17,13 @@ class Camera(models.Model):
 	lat_lng = models.GeometryField(geography=True, default=0) # Sets geometry field points to geography in postgis
 	# Source Information:
 	source = models.CharField(max_length=30, null=False)
-	source_url = models.CharField(max_length=100, null=False) # URL of source (Not for image data!)
+	source_url = models.CharField(max_length=100, null=False) # URL of the provider of the source (NOT for image data!)
 	# Time Information:
 	date_added = models.DateTimeField(auto_now_add=True)
 	last_updated = models.DateTimeField() # Last known time a snapshot was downloaded
+	# Camera Types (Non_ip or IP)
+	CAMERA_TYPES = enumerate(['Non_ip', 'IP'])
+	camera_type = models.CharField(max_length=10, null=False, choices=CAMERA_TYPES)
 	# More Info:
 	description = models.CharField(max_length=100, null=True) # Description of the camera
 	is_video = models.BooleanField() # True if camera is a video stream 
@@ -27,3 +34,24 @@ class Camera(models.Model):
 	inactive = models.NullBooleanField() # True if data cannot be accessed from the camera Null if unknown.
 	resolution_w = models.PositiveIntegerField(null=True) # Resolution width determined automatically 
 	resolution_h = models.PositiveIntegerField(null=True) # Resolution height determined automatically 
+
+	# Image Retrieval objects:
+	# For more information see https://docs.djangoproject.com/en/1.10/ref/contrib/contenttypes/#generic-relations
+	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+	object_id = models.PositiveIntegerField()
+	retrieval_model = GenericForeignKey('content_type', 'object_id')
+
+	def __str__(self):
+		return "camera_id: {}, camera_type: {}, image_retrieval: {}".format(camera_id, camera_type, image_retrieval)
+
+class Non_IP(models.Model):
+	url = models.CharField(max_length=100, null=False) # URL to the image data 
+
+class IP(models.Model):
+	ip = models.CharField(max_length=44, null=False)
+	port = models.PositiveIntegerField(null=True)
+	brand = models.PositiveIntegerField(null=True)
+	model = models.PositiveIntegerField(null=True)
+	video_path = models.CharField(max_length=100, null=False)
+	image_path = models.CharField(max_length=100, null=False)
+	rtsp_path = models.CharField(max_length=100, null=False)
