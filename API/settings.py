28c1114b2bcc9ec8e58c586fd288ae0ab89e9781
Ryan Dailey <ryan@localhost.localdomain>
2017-03-21 13:10:25 -0700

    Moved import statement to bottom of settings.py

--- API/settings.py
+++ API/settings.py
@@ -5,21 +5,11 @@ CAM2API
 
 import os
 
-try:
-    from settings_local import *
-except ImportError:
-    try:
-        from settings_remote import *
-    except ImportError:
-        pass
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Root Project Directory
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 
 # Default Set of DEBUG is False
 DEBUG = False
@@ -104,3 +94,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# Import settings specific to deployment
+
+try:
+    from settings_local import *
+except ImportError:
+    try:
+        from settings_remote import *
+    except ImportError:
+        pass
