89fdfc586b167ffe1027a275f48d160c2194581b
Mrjimjr <dailey1@purdue.edu>
2017-04-27 14:44:49 -0400

    adding fix for lat_long

--- CAM2API/views.py
+++ CAM2API/views.py
@@ -33,7 +33,9 @@ class CameraList(APIView):
 		return: HTTP 201 if the data successfully saved in the database or HTTP 400 if
 				there was an error saving the camera information to the database
 		"""
-		print("DATA:", request.data)
+		print(request.data)
+		print()
+		print(request.data['float_lat'])
 		serializer = CameraSerializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
