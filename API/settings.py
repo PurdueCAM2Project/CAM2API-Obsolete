1714037c1042cd9935c770c079875dd1013d9832
Mrjimjr <dailey1@purdue.edu>
2017-05-31 18:05:57 -0400

    added 404 and 500 viviews and changed the header of the API interface

--- API/settings.py
+++ API/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'API.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['API/staticfiles/','API/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- CAM2API/urls.py
+++ CAM2API/urls.py
@@ -3,9 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from CAM2API import views
 
 urlpatterns = [
+    url(r'^$', views.CameraList.as_view()),
     url(r'^cameras/$', views.CameraList.as_view()),
     url(r'^cameras/(?P<cd>[0-9]+)/$', views.CameraDetail.as_view()),
-    #url(r'^cameras/ip/(?P<pk>[0-9]+)/$', views.)
 ]
 
 # Adds format patterns to the API
