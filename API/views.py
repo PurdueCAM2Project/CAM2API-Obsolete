515d6f9f9851d9bd3b7fd183e47b414d1be3a366
Mrjimjr <dailey1@purdue.edu>
2017-06-01 11:36:18 -0400

    removed _ in .html files

--- API/views.py
+++ API/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 def resource_not_found(request):
 	context = {'path': request.path}
-	response = render(request, 'resource_not_found.html', context)
+	response = render(request, 'resourceNotFound.html', context)
 	response.status_code = 404
 	return response
 
 
 def server_error(request):
-	response = render(request, 'server_error.html')
+	response = render(request, 'serverError.html')
 	response.status_code = 500
 	return response
