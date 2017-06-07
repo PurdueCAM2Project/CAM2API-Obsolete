cd2d960c251253e1c6b81616a98690f821d0594e
Mrjimjr <dailey1@purdue.edu>
2017-06-07 12:04:21 -0400

    coppied settings

--- API/settings_remote.py
+++ API/settings_remote.py
@@ -41,7 +41,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'API', 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # for /static/root/favicon.ico    
