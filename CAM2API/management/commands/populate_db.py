b5d83990d8ae0a6fb05e4a55f908d1ee9d7285de
mmalesev <malesevic.milos2@gmail.com>
2017-06-14 10:39:45 -0400

    Tabs vs spaces bug fixed

--- CAM2API/management/commands/populate_db.py
+++ CAM2API/management/commands/populate_db.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
 		non_ip_cam = Camera(camera_id='52', lat_lng=lat_lng, lat=35.6895, lng=139.6917, city='Tokyo', state='', country='Japan', source='google', source_url='www.google.com', last_updated=timezone.now(), description='This is a non-ip camera', is_video=True, framerate=0.3, outdoors=True, indoors=False, traffic=False, inactive=False, resolution_w=1920, resolution_h=1080, camera_type="Non_ip", retrieval_model=im_uri)
 		non_ip_cam.save()
 
-        im_ip = IP(ip='10.0.0.1', port=80)
+		im_ip = IP(ip='10.0.0.1', port=80)
 		lat_lng = GEOSGeometry('{ "type": "Point", "coordinates": [ 40.4259, 86.9081 ] }')
 		ip_cam = Camera(camera_id='16', lat_lng=lat_lng, lat=40.4259, lng=86.9081, city='West Lafayette', state='Indiana', country='United States', source='google', source_url='www.google.com', last_updated=timezone.now(), description='This is an ip camera', is_video=False, outdoors=False, indoors=True, traffic=True, inactive=True, resolution_w=1024, resolution_h=768, camera_type="IP", retrieval_model=im_ip)
 		ip_cam.save()
