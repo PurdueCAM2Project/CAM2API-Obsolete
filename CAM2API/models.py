731e7aa0e445a493594ec97f028acc2571721aa4
mmalesev <malesevic.milos2@gmail.com>
2017-06-23 12:08:13 -0400

    Cleaning up the models, making some fields optional

--- CAM2API/models.py
+++ CAM2API/models.py
@@ -9,7 +9,7 @@ import datetime
 
 # Camera Model
 class Non_IP(models.Model):
-	url = models.CharField(max_length=100, null=False, unique=True) # URL to the image data 
+	url = models.URLField(unique=True) # URL to the image data
 
 class IP(models.Model):
 	ip = models.GenericIPAddressField(unique=True)
@@ -18,16 +18,16 @@ class IP(models.Model):
 class Camera(models.Model):
 	camera_id = models.PositiveIntegerField(unique=True) # id from old database
 	# Geography: 
-	city = models.CharField(max_length=30)
+	city = models.CharField(max_length=30, null=True, blank=True)
 	state = models.CharField(max_length=12, null=True, blank=True)
-	country = models.CharField(max_length=50)
+	country = models.CharField(max_length=50, null=True, blank=True)
 	lat = models.FloatField(max_length=100)
 	lng = models.FloatField(max_length=100)
 	lat_lng = models.GeometryField(geography=True, null=False, blank=True) # Sets geometry field points to geography in postgis
 
 	# Source Information:
-	source = models.CharField(max_length=30)
-	source_url = models.CharField(max_length=100) # URL of the provider of the source (NOT for image data!)
+	source = models.CharField(max_length=100)
+	source_url = models.URLField() # URL of the provider of the source (NOT for image data!)
 	# Time Information:
 	date_added = models.DateTimeField(auto_now_add=True)
 	last_updated = models.DateTimeField(auto_now_add=True) # Last known time a snapshot was downloaded30
@@ -35,15 +35,15 @@ class Camera(models.Model):
 	CAMERA_TYPES = enumerate(['Non_IP', 'IP'])
 	camera_type = models.CharField(max_length=10, null=False, blank=True, choices=CAMERA_TYPES, default='Non_IP')
 	# More Info:
-	description = models.CharField(max_length=100, null=True) # Description of the camera
+	description = models.CharField(max_length=100, null=True, blank=True) # Description of the camera
 	is_video = models.BooleanField(default=False) # True if camera is a video stream
 	framerate = models.FloatField(null=True, blank=True) # Frame rate of the camera if known
 	outdoors = models.NullBooleanField() # True if camera is outdoors Null if unknown.
 	indoors = models.NullBooleanField() # True if the camera is indoors Null if unknown.
 	traffic = models.NullBooleanField() # True if the camera is a traffic camera Null if unknown.
 	inactive = models.NullBooleanField() # True if data cannot be accessed from the camera Null if unknown.
-	resolution_w = models.PositiveIntegerField(null=True) # Resolution width determined automatically 
-	resolution_h = models.PositiveIntegerField(null=True) # Resolution height determined automatically 
+	resolution_w = models.PositiveIntegerField(null=True, blank=True) # Resolution width determined automatically 
+	resolution_h = models.PositiveIntegerField(null=True, blank=True) # Resolution height determined automatically 
 	# Image Retrieval objects:
 	# For more information see https://docs.djangoproject.com/en/1.10/ref/contrib/contenttypes/#generic-relations
 	
